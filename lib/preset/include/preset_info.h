41c27ff2a9b5c4f2eeaaac445b41b18ca1bdfb28
Hao Guo <hao.guo03@stud.uni-goettingen.de>
2026-07-02 17:02:28 +0200

    v1.0.2

--- lib/preset/include/preset_info.h
+++ lib/preset/include/preset_info.h
@@ -3,7 +3,7 @@
 
 namespace preset {
 
-const std::string PRESET_VERSION = "v1.0.1 (15.06.2026)";
+const std::string PRESET_VERSION = "v1.0.2 (28.06.2026)";
 const std::string RAYLIB_VERSION = "6.0";
 
 }
